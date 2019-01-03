a3a46f9c50fb9b0fb33c54324e9b92bef5cd83a7
Eunice Min <euniceem1007@gmail.com>
2019-01-02 18:41:29 -1000

    done

--- vault.js
+++ vault.js
@@ -1,4 +1,18 @@
 'use strict';
-module.exports = function() {
-  
+module.exports = function () {
+  const password = {};
+  const setValue = function (key, value) {
+    password[key] = value;
+  };
+  const getValue = function (key) {
+    if (key === undefined || !password[key]) {
+      return null;
+    } else {
+      return password[key];
+    };
+  };
+  return {
+    setValue,
+    getValue
+  };
 };
